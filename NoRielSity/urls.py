77f3dc016e8e36d0d715240a4dbe4b7dcb0b1a52
Andrii <andri207sh@gmmail.com>
2026-02-14 02:44:35 +0200

    Відкри фото

--- NoRielSity/urls.py
+++ NoRielSity/urls.py
@@ -60,6 +60,5 @@ urlpatterns = [
     path("", include("listings.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
